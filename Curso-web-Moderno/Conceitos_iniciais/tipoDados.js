ea6a3738450d44587c02b81379f7f21b317a0d89
JayXani <112290478+JayXani@users.noreply.github.com>
2023-05-01 17:31:49 -0300

    Create and update a exercice

--- Curso-web-Moderno/Conceitos_iniciais/tipoDados.js
+++ Curso-web-Moderno/Conceitos_iniciais/tipoDados.js
@@ -1,2 +1,7 @@
 let numero = 45;
+let trocaDados = Number("01.20");
+let valor = 1.2345
+console.log(trocaDados)
 console.log(typeof numero)
+console.log(valor.toFixed(2))
+console.log(valor.toString(2)) //Transforma um valor numerico em uma string, porem retorna em binario (2)

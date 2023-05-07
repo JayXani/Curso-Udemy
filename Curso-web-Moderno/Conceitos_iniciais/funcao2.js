6f711aa4f471aff4f14da242ebced7581f82cf0b
JayXani <112290478+JayXani@users.noreply.github.com>
2023-05-07 14:40:37 -0300

    Create a new exercice function with arrow

---
+++ Curso-web-Moderno/Conceitos_iniciais/funcao2.js
@@ -0,0 +1,15 @@
+const imprimirSoma = function(a,b){
+    console.log(a + b)
+}
+
+imprimirSoma(2,3)
+
+const somar = (a,b) => { //função arrow
+    return a + b
+}
+
+console.log(somar(2,4))
+
+const subtracao = (a,b) => a-b //função arrow com retorno implicito
+
+console.log(subtracao(4,2))

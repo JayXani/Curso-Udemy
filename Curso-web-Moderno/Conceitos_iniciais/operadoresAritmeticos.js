06298d71dbe438b3944a687d37c12b8111754f55
JayXani <112290478+JayXani@users.noreply.github.com>
2023-05-12 08:51:18 -0300

    Create some news exercices of "Operadores"

---
+++ Curso-web-Moderno/Conceitos_iniciais/operadoresAritmeticos.js
@@ -0,0 +1,11 @@
+const [a,b,c] = [12,234,5]
+
+const soma = a+b+c
+const subtracao = a-b-c
+const divisao = a / b
+const modulo = a % 2
+
+console.log(soma)
+console.log(subtracao)
+console.log(divisao)
+console.log(modulo)
---
+++ Curso-web-Moderno/Conceitos_iniciais/operadoresRelacionais.js
@@ -0,0 +1,13 @@
+console.log('1)','1' == 1)
+console.log('2)','1' === 1)
+
+console.log('3)','1' != 1)
+console.log('4)','1' !== 1)
+
+const d1 = new Date(0)
+const d2 = new Date(0)
+
+console.log('5)', d1 == d2)
+console.log('6)', d1 === d2)
+console.log('7)', d1.getTime() == d2.getTime())
+console.log('8)', d1.getTime() === d2.getTime())

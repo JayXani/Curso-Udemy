4086a27c8c1ba208ebbfd4b642c41a45a652cf83
JayXani <112290478+JayXani@users.noreply.github.com>
2023-05-22 10:05:29 -0300

    Create and Update the project

---
+++ Curso-web-Moderno/Conceitos_iniciais/tratamentoDeErros.js
@@ -0,0 +1,20 @@
+function tratamento(erro){
+    
+    throw 'Erro !! \nNão é possivel colocar em maiúscula um atributo undefined.'
+}
+
+
+function imprimirNomeUpeer(obj){
+    try{
+        console.log(obj.name.toUpperCase() + '!!!');
+    } catch (e){
+        tratamento(e);
+    }finally{
+        console.log('Esta linha será executada de todo modo !')
+    }
+    
+}
+
+
+const objeto = {nome: 'Roberto'}
+imprimirNomeUpeer(objeto)

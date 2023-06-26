9d6ad1349d04601476b111e5c3430576bf46d68f
JayXani <112290478+JayXani@users.noreply.github.com>
2023-06-26 19:43:54 -0300

    Update exercice.

--- Curso-web-Moderno/Arrays/lista de exercicios (Arrays)/deleteObject.js
+++ Curso-web-Moderno/Arrays/lista de exercicios (Arrays)/deleteObject.js
@@ -1,12 +1,5 @@
-function modifyObject(object, deleted){
-    var entries = Object.entries(object).filter(([key,value]) => {
-        key != deleted
-    })
-    var copia = {}
-    entries.forEach(([key,value]) => {
-        copia[key] = value
-    })
+function removerPropriedade(objeto, nomeDaPropriedade) {
+    const copia = Object.assign({}, objeto)
+    delete copia[nomeDaPropriedade]
     return copia
 }
-console.log(modifyObject({a:1,b:2}, 'a'))
-

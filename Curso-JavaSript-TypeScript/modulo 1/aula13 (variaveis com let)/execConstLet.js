6347db1bc94dda42227439f848869d1ca9496b10
JayXani <112290478+JayXani@users.noreply.github.com>
2023-06-27 12:04:22 -0300

    Create and update exercices.

---
+++ Curso-JavaSript-TypeScript/modulo 1/aula13 (variaveis com let)/execConstLet.js
@@ -0,0 +1,9 @@
+const nome = 'Danilo'
+const sobrenome = 'Araújo'
+const idade = 19
+const peso = 57
+const altura = 1.81
+let imc = peso / (altura * altura)
+let anoNascimento = '17/03/2004'
+
+console.log(`${nome}${sobrenome} tem ${idade}, pesa ${peso} kg, tem ${altura} de altura e seu IMC é de: ${imc}.\nNasceu em ${anoNascimento}`)
---
+++ Curso-JavaSript-TypeScript/modulo 1/aula7(html e js)/index.js
@@ -0,0 +1,2 @@
+console.log('Meu nome é "Danilo". Estou aprendendo JavaScript ás', 10, 'da manhã')
+alert('Essa mensagem só aparecerá no WINDOW')

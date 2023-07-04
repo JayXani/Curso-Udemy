749d12f0114e06dc38f655cb3fe7d23c62f8b427
JayXani <112290478+JayXani@users.noreply.github.com>
2023-07-04 15:30:16 -0300

    Update project.

--- Curso-JavaSript-TypeScript/modulo 1/aula48 (exercicio com DATE)/assets/js/scriptMain.js
+++ Curso-JavaSript-TypeScript/modulo 1/aula48 (exercicio com DATE)/assets/js/scriptMain.js
@@ -4,63 +4,16 @@ const alterDateInDocument = _ => {
     selectedElement.innerHTML = formatDay()
 }
 const formatDay = _ => {
-    let dayOfWeekend = ''
-    const day = formatedDate.getDate()
+    let dayOfWeekend = ['Domingo','Segunda-feira','Terça-feira','Quarta-feira','Quinta-feira','Sexta-feira', 'Sabado']
+    const day = (formatedDate.getDate() <= 9)?'0'.concat(formatedDate.getDate()): formatedDate.getDate()
     const year = formatedDate.getFullYear()
     const hour = formatedDate.getHours()
     const minutes = formatedDate.getMinutes()
-
-    switch (formatedDate.getDay()) {
-        case 0:
-            dayOfWeekend = 'Domingo'
-            break
-        case 1:
-            dayOfWeekend = 'Segunda-feira'
-            break
-        case 2:
-            dayOfWeekend = 'Terça-feira'
-            break
-        case 3:
-            dayOfWeekend = 'Quarta-feira'
-            break
-        case 4:
-            dayOfWeekend = 'Quinta-feira'
-            break
-        case 5:
-            dayOfWeekend = 'Sexta-feira'
-            break
-        case 6:
-            dayOfWeekend = 'Sabado'
-    }
-    return `${dayOfWeekend}, ${day} de ${getMonthInBR()} de ${year} ${hour}:${minutes}`
+    return `${dayOfWeekend[formatedDate.getDay()]}, ${day} de ${getMonthInBR()} de ${year} ${hour}:${minutes}`
 }
 const getMonthInBR = _ => {
     const month = formatedDate.getMonth()
-    switch (month + 1) {
-        case 1:
-            return 'Janeiro'
-        case 2:
-            return 'Fevereiro'
-        case 3:
-            return 'Março'
-        case 4:
-            return 'Abril'
-        case 5:
-            return 'Maio'
-        case 6:
-            return 'Junho'
-        case 7:
-            return 'Julho'
-        case 8:
-            return 'Agosto'
-        case 9:
-            return 'Setembro'
-        case 10:
-            return 'Outubro'
-        case 11:
-            return 'Novembro'
-        case 12:
-            return 'Dezembro'
-    }
+    const stringsMonths = ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dexembro',]
+    return stringsMonths[month]
 }
 alterDateInDocument()

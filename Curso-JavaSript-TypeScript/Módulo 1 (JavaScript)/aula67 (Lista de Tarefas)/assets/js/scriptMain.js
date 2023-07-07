2ea166fc9cb0ed7f6061289520277115d8df666c
JayXani <112290478+JayXani@users.noreply.github.com>
2023-07-07 14:17:25 -0300

    Update of the project.

--- Curso-JavaSript-TypeScript/Módulo 1 (JavaScript)/aula67 (Lista de Tarefas)/assets/js/scriptMain.js
+++ Curso-JavaSript-TypeScript/Módulo 1 (JavaScript)/aula67 (Lista de Tarefas)/assets/js/scriptMain.js
@@ -1,11 +1,36 @@
-function getElement(){
-    const inputTask = document.querySelector('.input-task')
-    const buttonConfirmTask = document.querySelector('.btn-add-task')
-    const listOfTask = document.querySelector('.list-tasks')
-    buttonConfirmTask.addEventListener('click', _ => {
+const inputTask = document.querySelector('.input-task')
+const buttonConfirmTask = document.querySelector('.btn-add-task')
+const listOfTask = document.querySelector('.list-tasks')
 
-        if(!inputTask.value) return //Se estiver vazio, ele não faz nada
-        console.log(inputTask.value)
+const createNewTask = (task) => {
+    const li = document.createElement('li')
+    li.innerText = task
+    li.appendChild(createButtonClean(li))
+    listOfTask.appendChild(li)
+}
+const createButtonClean = li =>{
+    const button = document.createElement('button')
+    button.innerText = 'Apagar'
+    button.classList.add('Clean-Task')
+    return button
+}
+const clearTask = _ => {
+    inputTask.value = ''
+    inputTask.focus()
+}
+
+function getEvents() {
+    buttonConfirmTask.addEventListener('click', _ => {
+        if (!inputTask.value) return //Se estiver vazio, ele não faz nada
+        createNewTask(inputTask.value)
+        clearTask()
+    })
+    inputTask.addEventListener('keypress', event => {
+        if(event.keyCode === 13){
+            if(!inputTask.value) return
+            createNewTask(inputTask.value)
+            clearTask()
+        }   
     })
 }
-getElement()
+getEvents()

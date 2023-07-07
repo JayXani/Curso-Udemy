6fbe19e572bcfcf222a5d93e5d5895fc0e1c2637
JayXani <112290478+JayXani@users.noreply.github.com>
2023-07-07 16:37:59 -0300

    Update project.

--- Curso-JavaSript-TypeScript/Módulo 1 (JavaScript)/aula67 (Lista de Tarefas)/assets/js/scriptMain.js
+++ Curso-JavaSript-TypeScript/Módulo 1 (JavaScript)/aula67 (Lista de Tarefas)/assets/js/scriptMain.js
@@ -1,36 +1,65 @@
 const inputTask = document.querySelector('.input-task')
 const buttonConfirmTask = document.querySelector('.btn-add-task')
-const listOfTask = document.querySelector('.list-tasks')
+const liOfTask = document.querySelector('.list-tasks')
 
-const createNewTask = (task) => {
+const createNewTask = task => {
     const li = document.createElement('li')
     li.innerText = task
     li.appendChild(createButtonClean(li))
-    listOfTask.appendChild(li)
+    liOfTask.appendChild(li)
+    saveTaskInLocalStorag()
 }
-const createButtonClean = li =>{
+const createButtonClean = _ =>{
     const button = document.createElement('button')
     button.innerText = 'Apagar'
-    button.classList.add('Clean-Task')
+    button.setAttribute('class', 'btn-clean-task')
+    button.setAttribute('title', 'Apagar está tarefa')
     return button
 }
-const clearTask = _ => {
+const clearInputTask = _ => {
     inputTask.value = ''
     inputTask.focus()
+}
+const saveTaskInLocalStorag = _ => {
+    const tasks = liOfTask.querySelectorAll('li')
+    const listOfTasks = []
+    
+    for(let captureTask of tasks){
+        let textTask = captureTask.innerText
+        textTask = textTask.replace('Apagar', '') //Cleaning text of button
+        listOfTasks.push(textTask)
+    }
+    const tasksInJSON = JSON.stringify(listOfTasks)
+    localStorage.setItem('task', tasksInJSON)
+}
+const getTaskInStorage = _ => {
+    const getTask = localStorage.getItem('task')
+    const taskJsonToArray = JSON.parse(getTask)
+    for(let taskSaved of taskJsonToArray){
+        createNewTask(taskSaved)
+    }
 }
 
 function getEvents() {
     buttonConfirmTask.addEventListener('click', _ => {
         if (!inputTask.value) return //Se estiver vazio, ele não faz nada
         createNewTask(inputTask.value)
-        clearTask()
+        clearInputTask()
     })
     inputTask.addEventListener('keypress', event => {
         if(event.keyCode === 13){
             if(!inputTask.value) return
             createNewTask(inputTask.value)
-            clearTask()
+            clearInputTask()
         }   
+    })
+    document.addEventListener('click', event => {
+        const captureEvent = event.target
+        if(captureEvent.classList.contains('btn-clean-task')){
+            captureEvent.parentElement.remove(  )
+            saveTaskInLocalStorag() //to Clean at list the of local Storage
+        }
     })
 }
 getEvents()
+getTaskInStorage()

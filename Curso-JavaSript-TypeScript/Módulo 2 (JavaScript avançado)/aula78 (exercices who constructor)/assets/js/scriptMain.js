74a948000dd89149d7c43723ad35ce5fa6695b37
JayXani <112290478+JayXani@users.noreply.github.com>
2023-07-11 11:19:55 -0300

    Update project.

--- Curso-JavaSript-TypeScript/Módulo 2 (JavaScript avançado)/aula78 (exercices who constructor)/assets/js/scriptMain.js
+++ Curso-JavaSript-TypeScript/Módulo 2 (JavaScript avançado)/aula78 (exercices who constructor)/assets/js/scriptMain.js
@@ -3,18 +3,11 @@ function Calculator() {
     const getEvent = _ => {
         document.addEventListener('click', event => {
             const captureEvent = event.target
-            if (captureEvent.classList.contains('btn-num')) {
-                displayToInput(captureEvent.innerText)
-            }
-            if (captureEvent.classList.contains('btn-clear')) {
-                clearInput()
-            }
-            if(captureEvent.classList.contains('btn-del')){
-                deleteOneELement()
-            }
-            if(captureEvent.classList.contains('btn-equals')){
-                result()
-            }
+            if (captureEvent.classList.contains('btn-num')) displayToInput(captureEvent.innerText)
+            if (captureEvent.classList.contains('btn-clear')) clearInput()
+            if(captureEvent.classList.contains('btn-del')) deleteOneELement()
+            if(captureEvent.classList.contains('btn-equals')) result()
+            
         })
     }
     this.inicialized = _ => {
@@ -23,27 +16,24 @@ function Calculator() {
     }
     const displayToInput = value => {
         this.display.value += value
+        this.display.focus()    //Jogando o foco para o display, assim corrige o bug de estar selecionando a última tecla pressionada
     }
-    const clearInput = _ =>{
-        this.display.value = " "
-    }
-    const deleteOneELement = _ =>{
-        this.display.value = this.display.value.slice(0,-1)
-    }
+    const clearInput = _ => this.display.value = " " 
+    const deleteOneELement = _ => this.display.value = this.display.value.slice(0,-1)
+    
     const result = _ => {
         let valueOfInput = this.display.value
         valueOfInput = eval(valueOfInput)
         try{
-            if(!valueOfInput){
-                alert('Valores incorretos')
-            }
+            if(!valueOfInput) alert('Valores incorretos')
             this.display.value = valueOfInput
         } catch(e){
             alert('Valores incorretos')
+            return
         }
     }
     const clickInEnter = _ => {
-        this.display.addEventListener('keyup', event => {
+        this.display.addEventListener('keypress', event => {
             if(event.keyCode === 13){
                 result()
             }
